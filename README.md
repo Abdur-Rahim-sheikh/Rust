4a511e79982d5ac5d7ffc89a31ea6ff6fcc2e0a9
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-06-18 20:27:13 +0600

    refresh

---
+++ README.md
@@ -0,0 +1,3 @@
+## Rust tutorial follow
+
+Following this youtube [tutorial](https://www.youtube.com/playlist?list=PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8)
---
+++ hello_world/target/debug/deps/hello_world-088573bd13cd2d02.d
@@ -0,0 +1,5 @@
+/home/abir/Desktop/learning/learn_rust/hello_world/target/debug/deps/libhello_world-088573bd13cd2d02.rmeta: src/main.rs
+
+/home/abir/Desktop/learning/learn_rust/hello_world/target/debug/deps/hello_world-088573bd13cd2d02.d: src/main.rs
+
+src/main.rs:
