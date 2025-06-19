d5dd6106a93f4ada5b7e3b4e2595f7a911d2e3ee
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-06-19 15:51:42 +0600

    but we

--- memory_allocation/src/mutable.rs
+++ memory_allocation/src/mutable.rs
@@ -1,7 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
-    let r1 = &mut s;
-    let r2 = &mut s;
-
-    println!("{}, {}", r1, r2)
+    let r1 = &s;
+    let r2 = &s;
+    // let r3 = &mut s; // this is not possible as r1 is still used below, which is in scope
+    println!("{}, {}", r1, r2);
+    let r3 = &mut s;
+    println!("{}", r3); // but this is possible. as no r1, r2 scope is up
 }
