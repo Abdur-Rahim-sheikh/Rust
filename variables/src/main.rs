9117f22f98b1750fc76a12a6c3b2acd761ec663a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-06-19 11:47:13 +0600

    shadow and tuple

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // let x = 5;
+    // println!("The value of x is: {}", x);
+    // let x = "six";
+    // println!("The value of x is: {}", x);
+    // const SUBSCRIBER_COUNT: u32 = 100_000;
+    // println!("The value of x is: {}", SUBSCRIBER_COUNT);
+    let tup = ("lets Get Rusty",100_000);
+    println!("The channel name {}",tup.0);
+}
