a492fd34da393a8c7adee86586e36034f3e749dd
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-06-19 10:41:00 +0600

    functional guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,35 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use colored::*;
+
 fn main() {
     println!("Guess the number");
     let secret_number = rand::rng().random_range(1..101);
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    loop{
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faild to read line");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessd: {}", guess);
-    let msg = match guess.cmp(&secret_number) {
-        Ordering::Less => "Too Small",
-        Ordering::Equal => "You win!",
-        Ordering::Greater => "Too big",
-    };
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_) => continue,
+        };
+        println!("You guessd: {}", guess);
+        let msg = match guess.cmp(&secret_number) {
+            Ordering::Less => "Too Small".red(),
+            Ordering::Equal => "You win!".green(),
+            Ordering::Greater => "Too big".yellow(),
+        };
 
-    println!("{}", msg);
+        println!("{}", msg);
+        if msg=="You win!".green(){
+            break;
+        }
+    }
+    
 }
