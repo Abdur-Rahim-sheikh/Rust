1247296f7881035f0a5f21c71b98191d009ea704
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-05 14:35:58 +0600

    generic type

---
+++ generic/src/main.rs
@@ -0,0 +1,23 @@
+use std::vec;
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let largest = get_largest(number_list);
+
+    println!("The largest number is {largest}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let largest_char = get_largest(char_list);
+    println!("The largest char is {largest_char}");
+}
+
+fn get_largest<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
+    let mut largest = number_list[0];
+    
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    return largest;
+}
