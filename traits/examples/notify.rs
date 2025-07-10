37dc8e9132677f6886b3cdf3a75b4084a19de12c
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-10 15:59:24 +0600

    notify

--- traits/examples/notify.rs
+++ traits/examples/notify.rs
@@ -5,6 +5,9 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        return format!("{}", self.author);
+    }
     // fn summarize(&self) -> String {
     //     return format!("{}, by {}", self.headline, self.author);
     // }
@@ -18,17 +21,26 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        return format!("@{}", self.username);
+    }
     fn summarize(&self) -> String {
         return format!("{}: {}", self.username, self.content);
     }
 }
 
 pub trait Summary {
+    fn summarize_author(&self) -> String;
     fn summarize(&self) -> String {
-        return String::from("Read more.... ");
+        return format!("Read more from {}... ", self.summarize_author());
     }
 }
 
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news {}", item.summarize());
+}
+
+pub fn notify2<T: Summary>(item1: &T, item2: &T) {}
 fn main() {
     let tweet = Tweet {
         username: String::from("abir"),
@@ -42,7 +54,7 @@ fn main() {
         headline: String::from("The sky is falling"),
         content: String::from("The sky is not actually falling"),
     };
-
+    notify(&article);
     println!("Tweet summary: {}", tweet.summarize());
     println!("Article summary: {}", article.summarize());
 }
