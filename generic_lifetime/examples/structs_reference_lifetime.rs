582e657141a9c0318eb1eff121041df2b5ecb018
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-11 17:55:14 +0600

    declaring static lifetime

--- generic_lifetime/examples/structs_reference_lifetime.rs
+++ generic_lifetime/examples/structs_reference_lifetime.rs
@@ -2,6 +2,12 @@ struct ImportExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'b> ImportExcerpt<'b> {
+    fn return_part(&self, announcement: &str) -> &str {
+        println!("Attention please {announcement}");
+        return self.part;
+    }
+}
 fn main() {
     let novel = String::from("Call me Ismail. Some years ago....");
     let first_sentence = novel.split('.').next().expect("Could not find");
