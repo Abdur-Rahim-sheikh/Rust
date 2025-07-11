08d2002d52ae878bc6cc7def8f36776f4f7ce8d4
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-11 17:17:06 +0600

    generic lifetime
    generic lifetime says that count for the smallest lifetime of the passed reference. which resolves dangling situation

---
+++ generic_lifetime/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s1 = String::from("abcd");
+    let s2 = String::from("xyz");
+
+    let result = longest(s1.as_str(), s2.as_str());
+    println!("The longest string is {result}")
+}
+// &i32             // a reference
+// &'a i32          // a reference with an explicit lifetime
+// &'a mut i32      //a mutable reference with an explicit lifetime
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        return x;
+    }
+    return y;
+}
