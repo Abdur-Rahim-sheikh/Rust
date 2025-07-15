0810bb30ccb7c0c01e1b080d04b71d72133319dc
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-15 17:10:34 +0600

    panic test with expected panic message

--- testing/src/panic_check.rs
+++ testing/src/panic_check.rs
@@ -4,8 +4,10 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100 got {}", value)
+        if value < 1 {
+            panic!("guess greater than 1")
+        } else if value > 100 {
+            panic!("guess less than 100")
         }
         return Guess { value };
     }
@@ -16,7 +18,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "guess less than 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
