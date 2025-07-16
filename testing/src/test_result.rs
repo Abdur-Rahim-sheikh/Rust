9380f800c85fa79c74c48c3ba14aceded2ce4b95
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-16 16:54:48 +0600

    ignore test
    we can tag a test as #[ignore] if we want them to ignore in general but can ran with specific command
    
    ```bash
    cargo test -- --ignored
    ```

--- testing/src/test_result.rs
+++ testing/src/test_result.rs
@@ -2,10 +2,19 @@
 mod tests {
     #[test]
     fn it_works() -> Result<(), String> {
-        if 2 + 3 == 4 {
+        if 2 + 2 == 4 {
             Ok(())
         } else {
             Err(String::from("Two plus three is not equal four."))
         }
     }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(2 + 2, 4)
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {}
 }
